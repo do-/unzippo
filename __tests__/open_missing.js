7b2d0360f979451a211a956211c36264388cbaff
Dmitry Ovsyanko <do_@pochta.ru>
2025-02-14 19:59:18 +0300

    node-stream-zip updated, test fixed

--- __tests__/open_missing.js
+++ __tests__/open_missing.js
@@ -2,17 +2,6 @@ const zip = require ('../index.js')
 
 it ('open missing entry', async () => {
 
+	await expect (() => zip.open ('./__data__/test.xlsx', 'xxxl/sharedStrings.xml')).rejects.toEqual (Error ('Entry not found'))
 
-//	expect (zip.open ('./__data__/test.xlsx', 'xxxl/sharedStrings.xml')).rejects.toThrow ()
-
-
-	try {
-		let s = await zip.open ('./__data__/test.xlsx', 'xxxl/sharedStrings.xml')
-		expect (new Error ()).toBe ('We must not be here')
-	}
-	catch (x) {
-		expect (x).toBe ('Entry not found')
-	}
-
-
-});
+})
